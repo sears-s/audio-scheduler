d7a92011af68ada9af6e56fe1cb53bb1caff6384
Sears <sears-s@users.noreply.github.com>
2018-10-29 14:02:38 -0600

    Update Help window

--- Help.xaml.cs
+++ Help.xaml.cs
@@ -14,7 +14,9 @@
                 "When editing a template or schedule, double click the last row to add a new event, and to delete an event, with a row selected, press the Delete key.\n\n" +
                 "If you play a sound while another is playing, the first one will stop and the second will begin playing.\n\n" +
                 "The time is based off of the computer's clock.\n\n" +
-                "Sears Schulz '20";
+                "To use the Reveille, To the Colors, Retreat, National Anthem, and TAPS buttons, you must add a sound with the exact same name as the button.\n\n" +
+                "Sears Schulz '20 - ***REMOVED***\n" +
+                "https://github.com/sears-s/audio-scheduler";
         }
     }
 }
